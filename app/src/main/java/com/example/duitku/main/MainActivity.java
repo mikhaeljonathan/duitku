88378c2afcee28f777f4373f3ba6f8480e8fa9e3
lunatic7up <72300371+lunatic7up@users.noreply.github.com>
2021-01-16 23:09:20 +0700

    showcase

--- app/src/main/java/com/example/duitku/main/MainActivity.java
+++ app/src/main/java/com/example/duitku/main/MainActivity.java
@@ -1,10 +1,30 @@
 package com.example.duitku.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.duitku.R;
 import com.example.duitku.interfaces.UIView;
+import com.github.florent37.tutoshowcase.TutoShowcase;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Random;
+import java.util.UUID;
+
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +33,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         UIView mainActivityView = new MainActivityView(this);
         mainActivityView.setUpUI();
+        setUpShowCase();
+    }
+
+    private void setUpShowCase(){
+        ShowcaseConfig config = new ShowcaseConfig();
+
+        config.setDelay(500);
+
+        UUID uuid = UUID.randomUUID();
+        String randomUUIDString = uuid.toString();
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this, randomUUIDString);
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(findViewById(R.id.nav_transaction),
+                "This is the menu to see your daily, weekly, monthly transaction, wallet, and budget", "GOT IT");
+
+        sequence.addSequenceItem(findViewById(R.id.nav_report),
+                "This is the menu to see your transaction report", "GOT IT");
+
+        sequence.addSequenceItem(findViewById(R.id.main_add_fab),
+                "This is the button to add a daily transaction", "GOT IT");
+
+        sequence.addSequenceItem(findViewById(R.id.nav_article),
+                "This is the menu to see some article that you may need", "GOT IT");
+
+        sequence.addSequenceItem(findViewById(R.id.nav_account),
+                "This is the menu to see and edit your profile", "GOT IT");
+
+        sequence.start();
+        sequence.setOnItemDismissedListener(new MaterialShowcaseSequence.OnSequenceItemDismissedListener() {
+            @Override
+            public void onDismiss(MaterialShowcaseView itemView, int position) {
+                if(position==4) showSwipeable();
+            }
+        });
+    }
+
+    private void showSwipeable(){
+        TutoShowcase.from(this)
+                .setContentView(R.layout.showcase_swipeable)
+                .setFitsSystemWindows(true)
+                .on(R.id.textView)
+                .displaySwipableLeft()
+                .delayed(1000)
+                .animated(true)
+                .show();
     }
 
 }
