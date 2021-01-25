9288b15c27096886806ffe861883d2627af40a38
mikhaeljonathan <mikhael629@gmail.com>
2021-01-25 21:20:47 +0700

    viewcategories

--- app/src/main/java/com/example/duitku/category/ViewCategoriesActivity.java
+++ app/src/main/java/com/example/duitku/category/ViewCategoriesActivity.java
@@ -1,10 +1,14 @@
 package com.example.duitku.category;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.duitku.R;
+import com.example.duitku.category.pick.ViewCategoriesFragment;
+import com.example.duitku.database.DuitkuContract.CategoryEntry;
+import com.google.android.material.tabs.TabLayout;
 
 public class ViewCategoriesActivity extends AppCompatActivity {
 
@@ -12,5 +16,25 @@ public class ViewCategoriesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_categories);
+
+        setUpViewPager();
     }
+
+    private void setUpViewPager(){
+        ViewPager viewPager = findViewById(R.id.view_categories_viewpager);
+
+        ViewCategoriesAdapter adapter = new ViewCategoriesAdapter(getSupportFragmentManager());
+
+        adapter.addFrag(new ViewCategoriesFragment(CategoryEntry.TYPE_INCOME), "Income");
+        adapter.addFrag(new ViewCategoriesFragment(CategoryEntry.TYPE_EXPENSE), "Expense");
+
+        viewPager.setAdapter(adapter);
+        setUpTabLayout(viewPager);
+    }
+
+    private void setUpTabLayout(ViewPager viewPager) {
+        TabLayout tabLayout = findViewById(R.id.view_categories_viewpager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
 }
---
+++ app/src/main/java/com/example/duitku/category/pick/ViewCategoriesFragment.java
@@ -0,0 +1,44 @@
+package com.example.duitku.category.pick;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+public class ViewCategoriesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final String type;
+
+    public ViewCategoriesFragment(String type){
+        this.type = type;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
+}
