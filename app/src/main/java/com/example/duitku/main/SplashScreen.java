c419aac0923d7cb7aa0de585298b5e5d2318fedc
mikhaeljonathan <mikhael629@gmail.com>
2021-01-27 09:14:27 +0700

    splashscreen

---
+++ app/src/main/java/com/example/duitku/main/SplashScreen.java
@@ -0,0 +1,25 @@
+package com.example.duitku.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.duitku.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, 500);
+    }
+}
